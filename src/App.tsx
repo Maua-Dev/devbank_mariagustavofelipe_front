d64eacb1cedf8ea62916e2d84ed16391d43a0e85
gustavomineli <gustavo.mineli332@gmail.com>
2026-05-01 17:47:31 -0300

    feat: adiciona telas de saque e deposito + correção de tipos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { useState } from 'react';
 // @ts-ignore
 import './App.css';
+const notas: number[] = [2, 5, 10, 20, 50, 100, 200];
 
+const [qtd, setQtd] = useState<Record<number, number>>(
+  Object.fromEntries(notas.map((n) => [n, 0]))
+);
+
+const total = notas.reduce((acc, n) => acc + n * (qtd[n] ?? 0), 0);
+
+const alterar = (nota: number, delta: number) => {
+  setQtd((prev) => ({
+    ...prev,
+    [nota]: Math.max(0, (prev[nota] ?? 0) + delta),
+  }));
+};
 interface Usuario {
   name: string;
   agency: string;
@@ -10,7 +23,7 @@ interface Usuario {
 }
 
 function App() {
-  const [tela, setTela] = useState<'config' | 'conta' | 'extrato'>('config');
+  const [tela, setTela] = useState<'config' | 'conta' | 'extrato' | 'saque' | 'deposito'>('config');
   const [apiUrl, setApiUrl] = useState('');
   const [usuario, setUsuario] = useState<Usuario | null>(null);
 
@@ -77,11 +90,17 @@ function App() {
             <div className="grid-cards-verticais">
               <div className="card-acao-vertical">
                 <span className="material-symbols-outlined icone-grande">payments</span>
-                <p>Depositar</p>
+                <div className="card-acao-vertical" onClick={() => setTela('deposito')}>
+  <span className="material-symbols-outlined icone-grande">payments</span>
+  <p>Depositar</p>
+</div>
               </div>
               <div className="card-acao-vertical">
                 <span className="material-symbols-outlined icone-grande">savings</span>
-                <p>Sacar</p>
+         <div className="card-acao-vertical" onClick={() => setTela('saque')}>
+  <span className="material-symbols-outlined icone-grande">savings</span>
+  <p>Sacar</p>
+</div>
               </div>
               <div className="card-acao-vertical" onClick={() => setTela('extrato')}>
                 <span className="material-symbols-outlined icone-grande">history</span>
@@ -91,6 +110,101 @@ function App() {
           </main>
         </div>
       )}
+      {/*tela 3 sacar e depositar*/}
+      {tela === 'saque' && (
+  <div className="dashboard-container">
+
+    <header className="dashboard-header">
+      <div className="header-content">
+        <h1 className="logo-dashboard">DevBank</h1>
+
+        <div className="info-usuario-card">
+          <p>Nome: {usuario?.name}</p>
+          <p>Agência: {usuario?.agency}</p>
+          <p>Conta: {usuario?.account}</p>
+        </div>
+      </div>
+    </header>
+
+    <main className="dashboard-main">
+
+      <div className="balance">
+        <span>Saldo Atual: R$ {usuario?.current_balance}</span>
+        <span>Total saque: R$ {total}</span>
+      </div>
+
+      <div className="notes">
+        {notas.map((note) => (
+          <div key={note} className="note-card">
+
+            <div className="note">R${note}</div>
+
+            <div className="counter">
+              <span>Quantidade</span>
+              <button onClick={() => alterar(note, -1)}>-</button>
+              <span>{qtd[note]}</span>
+              <button onClick={() => alterar(note, 1)}>+</button>
+            </div>
+
+          </div>
+        ))}
+      </div>
+
+      <div className="actions">
+        <button onClick={() => setTela('conta')}>Voltar</button>
+        <button>Sacar</button>
+      </div>
+
+    </main>
+  </div>
+)}
+{tela === 'deposito' && (
+  <div className="dashboard-container">
+
+    <header className="dashboard-header">
+      <div className="header-content">
+        <h1 className="logo-dashboard">DevBank</h1>
+
+        <div className="info-usuario-card">
+          <p>Nome: {usuario?.name}</p>
+          <p>Agência: {usuario?.agency}</p>
+          <p>Conta: {usuario?.account}</p>
+        </div>
+      </div>
+    </header>
+
+    <main className="dashboard-main">
+
+      <div className="balance">
+        <span>Saldo Atual: R$ {usuario?.current_balance}</span>
+        <span>Total depósito: R$ {total}</span>
+      </div>
+
+      <div className="notes">
+        {notas.map((note) => (
+          <div key={note} className="note-card">
+
+            <div className="note">R${note}</div>
+
+            <div className="counter">
+              <span>Quantidade</span>
+              <button onClick={() => alterar(note, -1)}>-</button>
+              <span>{qtd[note]}</span>
+              <button onClick={() => alterar(note, 1)}>+</button>
+            </div>
+
+          </div>
+        ))}
+      </div>
+
+      <div className="actions">
+        <button onClick={() => setTela('conta')}>Voltar</button>
+        <button>Depositar</button>
+      </div>
+
+    </main>
+  </div>
+)}
 
       {/* TELA 3: REGISTRO (HEADER IGUAL AO DASHBOARD) */}
       {tela === 'extrato' && (
